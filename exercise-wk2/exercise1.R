495e4c7e554a75d5566532c9f21fe04767103753
Alexander Escalera <aoes@uw.edu>
2018-01-17 17:30:02 -0800

    add compare function

--- exercise-wk2/exercise1.R
+++ exercise-wk2/exercise1.R
@@ -44,8 +44,8 @@ CompareLength <- function(v1,v2) {
 
 
 # Pass two vectors of different length to your `CompareLength` function
-
-
+CompareLength(v1, v2)
+print(CompareLength(v1,v2)) 
 # Write a function `DescribeDifference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
 # "Your second vector is longer by N elements"
